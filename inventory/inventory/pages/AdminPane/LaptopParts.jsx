70c542fc73fce7b34a02b6f5bfc545e1f78d8eaa
Muhammad Ibrahim Nasir <21-CP-26@students.uettaxila.edu.pk>
2024-06-04 18:13:21 +0500

    files uploaded

---
+++ inventory/inventory/pages/AdminPane/LaptopParts.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+export default function LaptopParts() {
+  return (
+    <div>
+        <div className="p-16">Alaptop parts</div>
+    </div>
+  )
+}
